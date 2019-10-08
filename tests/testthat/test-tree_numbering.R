a851440005399b299b9c97d459d7eea5d2316f06
Martin Smith <martins@gmail.com>
2019-10-08 13:50:07 +0100

    test_that('ReorderTreeStrict works',

--- tests/testthat/test-tree_numbering.R
+++ tests/testthat/test-tree_numbering.R
@@ -6,3 +6,18 @@ test_that("replacement reorder functions work correctly", {
   expect_equal(Pruningwise(tree), ape::reorder.phylo(tree, 'pruningwise'))
   expect_equal(Postorder(tree), ape::reorder.phylo(tree, 'postorder'))
 })
+
+test_that('ReorderTreeStrict works', {
+  tree1 <- ape::read.tree(text = '(((1, 2), 3), (4, (5, 6)));')
+  tree2 <- RenumberTips(ape::read.tree(text = '(((6, 5), 4), (3, (1, 2)));'), 
+                        as.character(1:6))
+  tree3 <- RenumberTips(ape::read.tree(text = '((4, (5, 6)), ((1, 2), 3));'),
+                        as.character(1:6))
+  
+  expect_equal(
+    RenumberTreeStrict(tree1$edge[, 1], tree1$edge[, 2]),
+    RenumberTreeStrict(tree3$edge[, 1], tree3$edge[, 2]))
+  expect_equal(
+    RenumberTreeStrict(tree2$edge[, 1], tree2$edge[, 2]),
+    RenumberTreeStrict(tree3$edge[, 1], tree3$edge[, 2]))
+})
